051d6e7f19334ee72bb2859eee40f79dd41797a1
rene-neissl <74980335+rene-neissl@users.noreply.github.com>
2022-01-26 09:15:45 +0100

    Updated to ForgeCLI 2.0.7 and fixed app component

--- src/index.tsx
+++ src/index.tsx
@@ -4,33 +4,31 @@ import { SkillManagement } from './skill-management';
 import { UserManagement } from './user-management';
 // import { DebugComponent } from './debug';
 
-const App = () => {
-    const context = useProductContext();
-
-    if (context.moduleKey === "intelligent-swarming-app-core") {
-        return (
-            <ProjectPage>
-                <Fragment>
-                    <UserManagement />
-                    <SkillManagement />
-                    {/*
-                    The DebugComponent is only used during development. It provides neat features to clear the stored data and
-                    reset users and their skills. As of writing this, Atlassian Forge does not provide a mechanism for determine 
-                    the environment where the app is currently running. As a result, the component can not be disabled when in
-                    production environment, so it is commented out for now.
-                    */}
-                    {/* <DebugComponent /> */}
-                </Fragment>
-            </ProjectPage>
-        );
-    }
-    else if (context.moduleKey === "intelligent-swarming-app-collaboration") {
-        return (
-            <IssuePanel>
-                <Collaboration />
-            </IssuePanel>
-        )
-    }
+const WorkshopApp = () => {
+    return (
+        <ProjectPage>
+            <Fragment>
+                <UserManagement />
+                <SkillManagement />
+                {/*
+                The DebugComponent is only used during development. It provides neat features to clear the stored data and
+                reset users and their skills. As of writing this, Atlassian Forge does not provide a mechanism for determine 
+                the environment where the app is currently running. As a result, the component can not be disabled when in
+                production environment, so it is commented out for now.
+                */}
+                {/* <DebugComponent /> */}
+            </Fragment>
+        </ProjectPage>
+    );
 };
 
-export const run = render(<App />);
+const CollabArea = () => {
+    return (
+        <IssuePanel>
+            <Collaboration />
+        </IssuePanel>
+    )
+};
+
+export const workshop = render(<WorkshopApp />);
+export const collabArea = render(<CollabArea />);
